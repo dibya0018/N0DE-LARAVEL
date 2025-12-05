47b344fd13cbcf57cf70cea5c7696de163a9d72f
Dibya Jyoti Datta <dibyajyotidatta777@gmail.com>
2025-12-05 12:20:05 +0530

    Delete Elmapi3 directory

--- Elmapi3/resources/js/components/app-logo-icon.tsx
+++
@@ -1,9 +0,0 @@
-import { ImgHTMLAttributes } from 'react';
-import { usePage } from '@inertiajs/react';
-import { type SharedData } from '@/types/index.d';
-
-export default function AppLogoIcon(props: ImgHTMLAttributes<HTMLImageElement>) {
-    const { branding } = usePage<SharedData>().props;
-    const src = branding.logo_url || '/logo.svg';
-    return <img src={src} {...props} />;
-}
